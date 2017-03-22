d6deb40ffbc9daeb96e4104f1bb00c04727eabbd
Attila Mihaly <attila.mihaly.jr@gmail.com>
2017-03-21 21:30:31 -0400

    Added LazyAst and test.

---
+++ src/main/scala/franka/lang/LazyAst.scala
@@ -0,0 +1,7 @@
+package franka.lang
+
+object LazyAst extends Ast {
+
+    type Data = Nothing
+
+}
---
+++ src/test/scala/franka/lang/LazyAstTest.scala
@@ -0,0 +1,13 @@
+package franka.lang
+
+class LazyAstTest {
+
+    import LazyAst._
+
+    val zero =
+        Select (Ident ('sdk), 'zero)
+
+//    val test =
+//        Literal (???)
+
+}
